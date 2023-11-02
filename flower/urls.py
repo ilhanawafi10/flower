abd7d8f7b50f9ac35d446a259314ee5b8e4854f5
ilhanawafi10 <ilhanawafi6@gmail.com>
2023-11-02 19:08:15 +0700

    second commit

--- flower/urls.py
+++ flower/urls.py
@@ -19,6 +19,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name="index"),
-    path('flower', views.flower, name="flower"),
+    path('/', views.index, name="index"),
+    path('flower/', views.flower, name="flower"),
 ]
